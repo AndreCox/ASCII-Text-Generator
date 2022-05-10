00108148c69eea3f26876810438348bc55ec4410
Andre <arc.xoc@gmail.com>
2022-05-09 17:21:55 -0700

    Updates

--- dist/Components/Button.js
+++ dist/Components/Button.js
@@ -2,7 +2,7 @@ import React from "../../_snowpack/pkg/react.js";
 function Button(props) {
   return /* @__PURE__ */ React.createElement("button", {
     type: "button",
-    className: "shadow shadow-green-400 even:text-green-400 border rounded-lg p-2 border-green-400",
+    className: "shadow text-green-400 shadow-green-400 even:text-green-400 border rounded-lg p-2 border-green-400",
     onClick: props.onClick
   }, props.children);
 }
